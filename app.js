7dd346c710fc57d7d79d4a40f66c5d52a58fba92
Suliman Alsarraf <suliman2@pdx.edu>
2020-02-23 16:39:09 -0800

    POST request handler now adds user data into the database upon user submission

--- app.js
+++ app.js
@@ -28,11 +28,16 @@ app.get('/', (req, res) => {
 	// serve public/index.html
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 app.post("/submit", function(req, res){
-  console.log('@ app.post')
-  // get username from the HTML form via request body
-  var username = req.body.username;
-  console.log(username)
-  res.redirect("/")
+	console.log('@ app.post')
+
+	// get username from the HTML form via request body
+	var username = req.body.username;
+
+	// initite SQL statment to be used for insertion
+	let statement = db.prepare("INSERT INTO users VALUES (?)");
+	statement.run(username)
+	console.log('Username: ' + username + ' was added to the database')
+	res.redirect("/")
 });
 
 app.listen(5000);

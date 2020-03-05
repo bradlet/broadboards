67fdd21ced9dcfe7bb38ff88bed195d2d47bd355
cteters <cteters@gmail.com>
2020-03-04 18:26:31 -0800

    Added in a basic thread posting front-end layout

--- broadboards/src/index.js
+++ broadboards/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
+import NewThread from "./js/thread.js";
 
 render(<NewThread />, document.getElementById("post"));
 render(<App />, document.getElementById("root"));

2641b92dcd25005e038a04cf70f3ad03202fc578
cteters <cteters@gmail.com>
2020-03-06 21:20:16 -0800

    Updated the dark & light themes more

--- broadboards/public/js/bb.js
+++ broadboards/public/js/bb.js
@@ -1,11 +1,13 @@
 function toggleDarkMode() {
   var body = document.getElementById("body");
   var currentClass = body.className;
-  var image = document.getElementById("logo");
+  var button = document.getElementById("dark-button");
 
   if (currentClass == "dark-mode") {
+    button.src = "fa fa-toggle-on";
     body.className = "light-mode";
   } else {
+    button.src = "fa fa-toggle-off";
     body.className = "dark-mode";
   }
 }

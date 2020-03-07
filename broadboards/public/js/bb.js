28acb2e8ce5a5d23995286318945c3be243e60bb
cteters <cteters@gmail.com>
2020-03-06 16:37:28 -0800

    Modified dark mode color scheme

--- broadboards/public/js/bb.js
+++ broadboards/public/js/bb.js
@@ -1,13 +1,11 @@
 function toggleDarkMode() {
-    var body = document.getElementById("body");
-    var currentClass = body.className;
-    var image = document.getElementById("logo");
-  
-    if (currentClass == "dark-mode") {
-      image.src = "img/bb.png";
-      body.className = "light-mode";
-    } else {
-      image.src = "img/bb-dark.png";
-      body.className = "dark-mode";
-    }
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  var image = document.getElementById("logo");
+
+  if (currentClass == "dark-mode") {
+    body.className = "light-mode";
+  } else {
+    body.className = "dark-mode";
   }
+}

53e25fdec927744c0acbe44996b8f8690ded669b
Suliman Alsarraf <suliman2@pdx.edu>
2020-03-11 06:02:44 -0700

    Add comments for thread.js and loginArea.js

--- broadboards/src/js/thread.js
+++ broadboards/src/js/thread.js
@@ -1,19 +1,33 @@
+/*
+  FILENAME: thread.js
+  PURPOSE:
+    JavaScript file that uses React to control the
+    thread posting mechanism. This includes:
+      1) Create a thread button that display input fields
+      upon press
+      2) Send a thread to the API endpoint
+*/
+
 import React from "react";
 import "./../textbox.css";
 
 // API routes
 const postThreadAPI = "/postThread";
 
+// main class that controls create a thread mechanism
 class NewThread extends React.Component {
   constructor(props) {
     super(props);
+    // default state: no new thread, no thread submission
     this.state = {
       NewThreadVisible: false,
       submitted: false
     };
+    // this binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // click handler that changes submission status upon press
   handleClick() {
     this.setState({
       submitted: true

253fdaef01e336c21995d189e0f1099c0887087d
Suliman Alsarraf <suliman2@pdx.edu>
2020-03-13 05:02:48 -0700

    Create broadboards tables

--- database_creation.js
+++ database_creation.js
@@ -16,3 +16,39 @@ const client = new Client({
 })
 client.connect()
 
+// create broadboards schema
+const schema = 'CREATE SCHEMA "broadboards"';
+client
+	.query(schema)
+	.then(res => console.log(res))
+	.catch(err => console.log(err))
+
+// create user table
+const createUserTable = 'CREATE TABLE "broadboards".user (' +
+		'username VARCHAR(20) PRIMARY KEY, joined timestamp, ' +
+		'email VARCHAR(64), password VARCHAR(100));'
+client
+	.query(createUserTable)
+	.then(res => console.log(res))
+	.catch(err => console.log(err))
+
+// create thread table
+const createThreadTable = 'CREATE TABLE "broadboards".thread (' +
+		'id SERIAL PRIMARY KEY, title VARCHAR(250), ' +
+		'content VARCHAR, created timestamp, ' +
+		'username VARCHAR(20));'
+client
+	.query(createThreadTable)
+	.then(res => console.log(res))
+	.catch(err => console.log(err))
+
+// create post table
+const createPostTable = 'CREATE TABLE "broadboards".post (' +
+		'id SERIAL PRIMARY KEY, response VARCHAR, ' +
+		'created timestamp, thread_id int, ' +
+		'username VARCHAR(20));'
+client
+	.query(createPostTable)
+	.then(res => console.log(res))
+	.catch(err => console.log(err))
+

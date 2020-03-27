d2914ac050aa0923799e38eb44c2b19dfe1862f5
Bradley Thompson <bradlet2@pdx.edu>
2020-03-27 09:07:45 -0700

    Reformat code with prettier

--- account_control.js
+++ account_control.js
@@ -1,13 +1,13 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 module.exports = {
-	encryptPW: function(pass) {
+  encryptPW: function(pass) {
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(pass, salt);
     return hash;
-    },
-    checkPW: function(pass, hash) {
-        return bcrypt.compareSync(pass, hash);
-    }
-}
+  },
+  checkPW: function(pass, hash) {
+    return bcrypt.compareSync(pass, hash);
+  }
+};
--- broadboards/public/js/login.js
+++ broadboards/public/js/login.js
@@ -2,20 +2,20 @@
 // login.html's submit button.
 
 // API routes
-const checkUsernameAPI = "/checkUsernameExists/"
-const checkPWAPI = "/checkPassword"
+const checkUsernameAPI = "/checkUsernameExists/";
+const checkPWAPI = "/checkPassword";
 
 const attemptLogin = function() {
-    /*
-    const response = await fetch(checkUsernameAPI+
-      '/'+startingNumOfThreads);
-    // console.log(response)
-    const body = await response.json();
-    // console.log(body)
+  /*
+  const response = await fetch(checkUsernameAPI+
+    '/'+startingNumOfThreads);
+  // console.log(response)
+  const body = await response.json();
+  // console.log(body)
 
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-    */
-}
+  if (response.status !== 200) {
+    throw Error(body.message)
+  }
+  return body;
+  */
+};
